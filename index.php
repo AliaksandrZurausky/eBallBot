b936e9988dce2bce1c04d54fab0f87e8e6991ee5
Aliaxander Zhurauskiu <sasha.zurausky@gmail.com>
2025-01-25 13:32:23 +0300

    index.php v1.0.1

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ $data = file_get_contents('php://input');
 $data = json_decode($data, true);
 function writeLogFile($string, $clear = false){
     $log_file_name = __DIR__."/message.txt";
+    $now = date("Y-m-d H:i:s");
     if($clear = false){
-        $now = date("Y-m-d H:i:s");
         file_put_contents($log_file_name, $now ." ". print_r($string, true). "\r\n", FILE_APPEND);
     }
     else{
