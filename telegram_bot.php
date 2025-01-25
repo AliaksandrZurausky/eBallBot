a8848a3bf7f7472f62aa5e2137255929deefc9b3
Aliaxander Zhurauskiu <sasha.zurausky@gmail.com>
2025-01-25 13:13:32 +0300

    ADD index.php

---
+++ telegram_bot.php
@@ -0,0 +1,11 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+const TG_TOKEN = "7796321296:AAGF3pL1raIZ1iIL7kA7iDLPXoqkOUS8X2s";
+const TG_USER_ID = "1783624604";
+
+$getQuery = [
+    "url" => "https://test.bots-time.ru/index.php"
+];
+?>
